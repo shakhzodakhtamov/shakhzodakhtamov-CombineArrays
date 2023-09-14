e6b7d70fc235d4fa79f40856c768a8ea882d9851
Shakhzod Akhtamov <shakakhtamov@gmail.com>
2023-09-14 23:23:08 +0000

    CombineArrays complete

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -8,7 +8,17 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
+        int al=arr1.length;
+        int bl=arr2.length;
+        int cl=al+bl;
+        int[] newarr= new int[cl];
+        for(int i =0; arr1.length>i;i++){
+            newarr[i]=arr1[i];
+        }
+        for(int i =0; arr2.length>i;i++){
+            newarr[arr1.length+i]=arr2[i];
+        }
 
-        return null;
+        return newarr;
     }
 }
